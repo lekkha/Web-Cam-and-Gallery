4e9675d2606d074564b51d62da5090d27e2b3464
Lekha <lekhasoni001@gmail.com>
2023-12-23 22:05:38 +0530

    video recording

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
-let video = document.querySelector("video"); 
+
+
+let video = document.querySelector("video");
+let recordBtnCont = document.querySelector(".record-btn-cont");
+let recordBtn = document.querySelector(".record-btn");
+let captureBtnCont = document.querySelector(".capture-btn-cont");
+let captureBtn = document.querySelector(".capture-btn");
+let recordFlag = false;
+
+
+let recorder;
+
 
 let constraints = {
-    video: true, 
+    video: true,
     audio: true
 }
 
-// navigator.mediaDevices.getUserMedia(constraints)
-// .then((stream) => {
-//     video.srcObject = stream; 
-// })
+// navigator -> global, browser info
+navigator.mediaDevices.getUserMedia(constraints)
+    .then((stream) => {
+        video.srcObject = stream;
 
+        recorder = new MediaRecorder(stream);
+    })
+
+recordBtnCont.addEventListener("click", (e) => {
+    if (!recorder) return;
+
+    recordFlag = !recordFlag;
+
+    if (recordFlag) { // start
+        recorder.start();
+        recordBtn.classList.add("scale-record");
+        
+    }
+    else { // stop
+        recorder.stop();
+        recordBtn.classList.remove("scale-record");   
+    }
+})
 
